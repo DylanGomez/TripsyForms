1aa96fd857f35d90bbae7a463ab820d6e34b7ec4
Dylan Gomez <dylangomez@overpridmachine.home>
2019-06-24 14:17:44 +0200

    tried another routing mec

--- src/Constants/routes.js
+++ src/Constants/routes.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {
     BrowserRouter as Router,
-    HashRouter as TestRouter,
     Route,
 } from 'react-router-dom'
 import SendForm from '../Components/form/SendForm/SendForm';
@@ -11,12 +10,13 @@ import SuccessPage from '../Components/form/SuccesPage/SuccessPage';
 
 
 const RouterForms = () => (
-    <TestRouter >
+    <Router basename={process.env.PUBLIC_URL}>
         <div>
             <Route path="/main/:destination" component={MainScreen} />
             <Route path="/sendform" component={SendForm} />
             <Route path="/success" component={SuccessPage} />
+            <Route component={() => (<div>404 Not found </div>)} />
         </div>
-    </TestRouter>
+    </Router>
 )
 export default RouterForms
