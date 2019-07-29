76a00f736b5defdf86c0baab1bc9d8f97914db90
Dylan Gomez <dylangomez@Overpriced-machine.local>
2019-07-29 15:31:55 +0200

    added form control

--- src/Components/form/SendForm/SendForm.js
+++ src/Components/form/SendForm/SendForm.js
@@ -8,8 +8,6 @@ import MediaQuery from 'react-responsive';
 import * as emailjs from 'emailjs-com';
 import { Redirect } from 'react-router-dom';
 
-
-
 class SendForm extends Component {
     constructor() {
         super();
@@ -21,7 +19,8 @@ class SendForm extends Component {
             phoneNumber: "",
             formInfo: {},
             helpOpen: false,
-            buttonPressed: false
+            buttonPressed: false,
+            validated: false,
         }
         this.toggleHelp = this.toggleHelp.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -65,80 +64,90 @@ class SendForm extends Component {
                 return res;
             }
         }).catch((error) => {
-            console.log("test");
+            console.log(error)
         })
     }
 
     mailService(event) {
-        event.preventDefault();
+        const form = event.currentTarget;
+        var check = false;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+        } else {
+            this.setState({
+                buttonPressed: true,
+            });
+            check = true;
+        }
 
         this.setState({
-            buttonPressed: true
+            validated: true
         });
 
-        var customerTemplate = {
-            from_name: 'hallo@tripsy.nl',
-            to_name: this.state.email,
-            subject: this.state.name,
-            message_html: 'Beste ' + this.state.name + ', <br> ' +
-                '<br>' +
-                'Bedankt voor je aanvraag! ' +
-                '<br>' +
-                '<br>' +
-                'Wij nemen zo spoedig mogelijk contact met je op om een telefonische afspraak te plannen. Dit is nodig om een reisplan op maat voor je te kunnen maken die in alle opzichten spectaculair is! <br>' +
-                '<br>' +
-                'Mocht je binnen 1 werkdag niks van ons hebben gehoord dan is er bij ons iets mis gegaan. Laat het ons alsjeblieft weten want we komen alsnog graag met je in contact.' +
-                '<br>' +
-                '<br>' +
-                'Met vriendelijke groet, ' +
-                'Het Tripsy team' +
-                '<br>' +
-                '<br>' +
-                'Bellen: 030 - 711 68 97 <br>' +
-                'Mailen: hallo@tripsy.nl <br>' +
-                'Website: www.tripsy.nl <br>' +
-                '<br>' +
-                '<br>' +
+        if (check) {
+            var customerTemplate = {
+                from_name: 'hallo@tripsy.nl',
+                to_name: this.state.email,
+                subject: this.state.name,
+                message_html: 'Beste ' + this.state.name + ', <br> ' +
+                    '<br>' +
+                    'Bedankt voor je aanvraag! ' +
+                    '<br>' +
+                    '<br>' +
+                    'Wij nemen zo spoedig mogelijk contact met je op om een telefonische afspraak te plannen. Dit is nodig om een reisplan op maat voor je te kunnen maken die in alle opzichten spectaculair is! <br>' +
+                    '<br>' +
+                    'Mocht je binnen 1 werkdag niks van ons hebben gehoord dan is er bij ons iets mis gegaan. Laat het ons alsjeblieft weten want we komen alsnog graag met je in contact.' +
+                    '<br>' +
+                    '<br>' +
+                    'Met vriendelijke groet, ' +
+                    'Het Tripsy team' +
+                    '<br>' +
+                    '<br>' +
+                    'Bellen: 030 - 711 68 97 <br>' +
+                    'Mailen: hallo@tripsy.nl <br>' +
+                    'Website: www.tripsy.nl <br>' +
+                    '<br>' +
+                    '<br>' +
 
-                '<br>' +
-                '<br>'
+                    '<br>' +
+                    '<br>'
 
+            }
+            var tripsyTemplate = {
+                from_name: 'hallo@tripsy.nl',
+                to_name: 'hallo@tripsy.nl',
+                subject: 'Bevestiging aanvraag',
+                message_html: '<b>AANVRAAG REISVOORSTEL: Thailand </b> ' +
+                    '<br> <br> <b>Aanvrager voornaam:</b> ' + this.state.name + ' <br> ' +
+                    '<b>Achternaam:</b> ' + this.state.lastname + '<br> ' +
+                    '<b>E-mailadres:</b> ' + this.state.email + '<br>  ' +
+                    '<b>Telefoonnummer:</b> ' + this.state.phoneNumber + '<br> <br> <br> ' +
+
+                    '<b>Met wie wil je op reis: </b>' + this.state.formInfo.state.who + '<br>' +
+                    '<b>Met hoeveel personen wil je op reis: </b>' + JSON.stringify(this.state.formInfo.state.howMany) + '<br>' +
+                    '<b>Hoe lang wil je op reis: </b> ' + this.state.formInfo.state.weeks + ' dagen<br>' +
+                    '<b>Wanneer wil je op reis: </b> ' + this.state.formInfo.state.when + ' <br>' +
+                    '<b>In welke snelheid wil je reizen: </b> ' + this.state.formInfo.state.speed + ' <br>' +
+                    '<b>Wat wil je het liefst ervaren:</b> ' + this.state.formInfo.state.activity + '<br>' +
+                    '<b>Voorkeur voor accomodatie:</b> ' + this.state.formInfo.state.accomodation + '<br>' +
+                    '<b>Optioneel: Met welke groep:</b> ' + this.state.formInfo.state.group + '<br>' +
+                    + '<br>' +
+                    'Met vriendelijke groet,<br> Het Tripsy Team <br> <br>' +
+                    'Bellen: +31 (0)30 - 711 62 47 <br>' +
+                    'Mailen: hallo@tripsy.nl<br>' +
+                    'Website: www.tripsy.nl<br> <br>'
+            }
+            var _this = this;
+
+            _this.props.history.push("/success")
+
+            emailjs.send('gmail', 'customertemplate', customerTemplate, 'user_f1aefYYUBoh4CwEBL8rCN')
+                .then(function (response) {
+                    emailjs.send('gmail', 'tripsytemplate', tripsyTemplate, 'user_f1aefYYUBoh4CwEBL8rCN')
+                }, function (err) {
+                    console.log(err)
+                });
         }
-        var tripsyTemplate = {
-            from_name: 'hallo@tripsy.nl',
-            to_name: 'hallo@tripsy.nl',
-            subject: 'Bevestiging aanvraag',
-            message_html: '<b>AANVRAAG REISVOORSTEL: Thailand </b> ' +
-                '<br> <br> <b>Aanvrager voornaam:</b> ' + this.state.name + ' <br> ' +
-                '<b>Achternaam:</b> ' + this.state.lastname + '<br> ' +
-                '<b>E-mailadres:</b> ' + this.state.email + '<br>  ' +
-                '<b>Telefoonnummer:</b> ' + this.state.phoneNumber + '<br> <br> <br> ' +
-
-                '<b>Met wie wil je op reis: </b>' + this.state.formInfo.state.who + '<br>' +
-                '<b>Met hoeveel personen wil je op reis: </b>' + JSON.stringify(this.state.formInfo.state.howMany) + '<br>' +
-                '<b>Hoe lang wil je op reis: </b> ' + this.state.formInfo.state.weeks + ' dagen<br>' +
-                '<b>Wanneer wil je op reis: </b> ' + this.state.formInfo.state.when + ' <br>' +
-                '<b>In welke snelheid wil je reizen: </b> ' + this.state.formInfo.state.speed + ' <br>' +
-                '<b>Wat wil je het liefst ervaren:</b> ' + this.state.formInfo.state.activity + '<br>' +
-                '<b>Voorkeur voor accomodatie:</b> ' + this.state.formInfo.state.accomodation + '<br>' +
-                '<b>Optioneel: Met welke groep:</b> ' + this.state.formInfo.state.group + '<br>' +
-                + '<br>' +
-                'Met vriendelijke groet,<br> Het Tripsy Team <br> <br>' +
-                'Bellen: +31 (0)30 - 711 62 47 <br>' +
-                'Mailen: hallo@tripsy.nl<br>' +
-                'Website: www.tripsy.nl<br> <br>'
-        }
-        var _this = this;
-
-        emailjs.send('gmail', 'customertemplate', customerTemplate, 'user_f1aefYYUBoh4CwEBL8rCN')
-            .then(function (response) {
-                emailjs.send('gmail', 'tripsytemplate', tripsyTemplate, 'user_f1aefYYUBoh4CwEBL8rCN')
-                    .then(function (response) {
-                        _this.props.history.push("/success")
-                    });
-            }, function (err) {
-                console.log(err)
-            });
     }
 
     goToSucces() {
@@ -187,12 +196,12 @@ class SendForm extends Component {
             return (
                 <div className="pageDiv">
                     <div className="headerDiv">
-                    <a href="https://www.tripsy.nl/" target="_blank" without rel="noopener noreferrer"><div className="logo"> Tripsy </div></a>
-                    <div className="helpIcon" style={{ pointerEvents: this.state.helpOpen ? 'none' : 'fill' }} onClick={() => this.toggleHelp()}> <span className="helpword">Help</span>
-                        {this.state.helpOpen &&
-                            <HelpModal toggleHelp={this.toggleHelp} />
-                        }
-                    </div>
+                        <a href="https://www.tripsy.nl/" target="_blank" rel="noopener noreferrer"><div className="logo"> Tripsy </div></a>
+                        <div className="helpIcon" style={{ pointerEvents: this.state.helpOpen ? 'none' : 'fill' }} onClick={() => this.toggleHelp()}> <span className="helpword">Help</span>
+                            {this.state.helpOpen &&
+                                <HelpModal toggleHelp={this.toggleHelp} />
+                            }
+                        </div>
                     </div>
                     <div className="backgroundNew">
                         <div className="textDivSend">
@@ -204,47 +213,57 @@ class SendForm extends Component {
                             <MediaQuery query='(max-device-width: 1224px)'>
                                 <span className="subSendTripText">Wij maken je reis - gratis en vrijblijvend</span>
                             </MediaQuery>
-                            <div className="formArea">
-                                <div className="formAreaRight">
-                                    <span className="whoText">Wie mag het reisaanbod ontvangen?</span>
-                                    <br />
-                                    <div className="radio-buttons">
-                                        <div className="inputGroup">
-                                            <input onChange={this.changeHandler} value="Man" id="radio1" name="gender" type="radio" defaultChecked="checked" />
-                                            <label htmlFor="radio1" >Man</label>
+                            <Form noValidate validated={this.state.validated} onSubmit={this.mailService}>
+                                <div className="formArea">
+                                    <div className="formAreaRight">
+                                        <span className="whoText">Wie mag het reisaanbod ontvangen?</span>
+                                        <br />
+                                        <div className="radio-buttons">
+                                            <div className="inputGroup">
+                                                <input onChange={this.changeHandler} value="Man" id="radio1" name="gender" type="radio" defaultChecked="checked" />
+                                                <label htmlFor="radio1" >Man</label>
+                                            </div>
+                                            <div className="inputGroup">
+                                                <input onChange={this.changeHandler} value="Woman" id="radio2" name="gender" type="radio" />
+                                                <label htmlFor="radio2">mevr.</label>
+                                            </div>
                                         </div>
-                                        <div className="inputGroup">
-                                            <input onChange={this.changeHandler} value="Woman" id="radio2" name="gender" type="radio" />
-                                            <label htmlFor="radio2">mevr.</label>
-                                        </div>
+                                        <Form.Control className="formField" name="name" size="lg" type="text" placeholder="Voornaam" onChange={this.changeHandler} required />
+                                        <Form.Control.Feedback type="invalid">
+                                            Voer een naam in.
+                                        </Form.Control.Feedback>
+                                        <Form.Control className="formField" name="lastname" size="lg" type="text" placeholder="Achternaam" onChange={this.changeHandler} required />
+                                        <Form.Control.Feedback type="invalid">
+                                            Voer een achternaam in.
+                                        </Form.Control.Feedback>
+                                        <Form.Control className="formField" name="email" size="lg" type="text" placeholder="E-mailadres" onChange={this.changeHandler} required />
+                                        <Form.Control.Feedback type="invalid">
+                                            Voer een geldig e-mail adres in.
+                                        </Form.Control.Feedback>
+
+                                        <IntlTelInput
+                                            className="telephoneInput"
+                                            name="phoneNumber"
+                                            onPhoneNumberChange={(status, value, countryData, number, id) => {
+                                                this.onChange(number);
+                                            }}
+                                            preferredCountries={['nl']}
+                                            onlyCountries={['nl', 'be']}
+                                            required
+                                        />
+                                        {!this.state.buttonPressed &&
+                                            <Button variant="success" size="lg" type="submit">Ja, verstuur het reisaanbod</Button>
+                                        }
+                                        {this.state.buttonPressed &&
+                                            <Button variant="success" size="lg" disabled>
+                                                <Spinner animation="border" />
+                                            </Button>
+                                        }
+                                        <br />
+                                        <span><i>Gratis en vrijblijvend </i></span>
                                     </div>
-                                    <Form.Control className="formField" name="name" size="lg" type="text" placeholder="Voornaam" onChange={this.changeHandler} />
-
-                                    <Form.Control className="formField" name="lastname" size="lg" type="text" placeholder="Achternaam" onChange={this.changeHandler} />
-
-                                    <Form.Control className="formField" name="email" size="lg" type="text" placeholder="E-mailadres" onChange={this.changeHandler} />
-
-                                    <IntlTelInput
-                                        className="telephoneInput"
-                                        name="phoneNumber"
-                                        onPhoneNumberChange={(status, value, countryData, number, id) => {
-                                            this.onChange(number);
-                                        }}
-                                        preferredCountries={['nl']}
-                                        onlyCountries={['nl', 'be']}
-                                    />
-                                    {!this.state.buttonPressed &&
-                                        <Button variant="success" size="lg" onClick={this.mailService}>Ja, verstuur het reisaanbod</Button>
-                                    }
-                                    {this.state.buttonPressed &&
-                                        <Button variant="success" size="lg" disabled>
-                                            <Spinner animation="border" />
-                                        </Button>
-                                    }
-                                    <br />
-                                    <span><i>Gratis en vrijblijvend </i></span>
                                 </div>
-                            </div>
+                            </Form>
                         </div>
                     </div>
                     <div className="bottomDiv">
